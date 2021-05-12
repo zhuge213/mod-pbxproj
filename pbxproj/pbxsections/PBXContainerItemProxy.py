febf139b0ccc297eea7dd2af3dc39234c319570c
zhuge <>
2021-05-12 23:27:26 +0800

    some change

--- pbxproj/pbxsections/PBXContainerItemProxy.py
+++ pbxproj/pbxsections/PBXContainerItemProxy.py
@@ -3,14 +3,16 @@ from pbxproj import PBXGenericObject
 
 class PBXContainerItemProxy(PBXGenericObject):
     @classmethod
-    def create(cls, file_ref, remote_ref, proxy_type=2):
+    def create(cls, file_ref, remote_ref, proxy_type=2, remoteGlobalIDString=None):
+        if remoteGlobalIDString is None:
+            remoteGlobalIDString = remote_ref.productReference
         return cls().parse({
             '_id': cls._generate_id(),
             'isa': cls.__name__,
             'containerPortal': file_ref.get_id(),
             'proxyType': proxy_type,
-            'remoteGlobalIDString': remote_ref.productReference,
-            'remoteInfo': remote_ref.productName
+            'remoteGlobalIDString': remoteGlobalIDString,
+            'remoteInfo': remote_ref.name
         })
 
     def _get_comment(self):
--- pbxproj/pbxsections/PBXTargetDependency.py
+++ pbxproj/pbxsections/PBXTargetDependency.py
@@ -2,5 +2,14 @@ from pbxproj import PBXGenericObject
 
 
 class PBXTargetDependency(PBXGenericObject):
+    @classmethod
+    def create(cls, proxy_ref):
+        return cls().parse({
+            '_id': cls._generate_id(),
+            'isa': cls.__name__,
+            'name': proxy_ref.remoteInfo,
+            'targetProxy': proxy_ref.get_id()
+        })
+
     def _get_comment(self):
         return 'PBXTargetDependency'
